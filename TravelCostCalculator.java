3319ccb342f70624baefda7f7826ad329c602802
Achala16 <131660182+Achala16@users.noreply.github.com>
2023-09-22 11:00:41 +0000

    //EG.2020.3891

--- TravelCostCalculator.java
+++ TravelCostCalculator.java
@@ -1,9 +1,17 @@
+
+//EG.2020.3891
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
+
+
+
+
+
 
 public class TravelCostCalculator {
     static Map<String, Double> a = new HashMap<>();
@@ -45,27 +53,37 @@ public class TravelCostCalculator {
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-            System.out.print("Enter your destination: ");
-            String destination = reader.readLine().toUpperCase();
+            System.out.print("Enter your destination: ");//asking the user to enter the destination
+            String destination = reader.readLine().toUpperCase();//getting the entered value
 
-            double flight_cost = c.getOrDefault(destination, 0.0);
-            double hotel_cost = a.getOrDefault(destination, 0.0);
+            double flightCost;//variable to store flight cost
+            double hotelCost; //variable to store hotel cost
+
+             flightCost = c.getOrDefault(destination, 0.0);//to get flight cost
+             hotelCost = a.getOrDefault(destination, 0.0);//to get hotel cost
+
+
 
             System.out.print("Enter your stay duration in days: ");
-            int stay_duration = Integer.parseInt(reader.readLine());
-            hotel_cost *= stay_duration;
 
-            double total_cost_usd = flight_cost + hotel_cost;
+            int stayDuration;//variable to store duration that we stay in a place
 
-            System.out.printf("Flight cost: USD %.2f\n", flight_cost);
-            System.out.printf("Hotel cost (%d days): USD %.2f\n", stay_duration, hotel_cost);
-            System.out.printf("Total: USD %.2f\n", total_cost_usd);
+            stayDuration = Integer.parseInt(reader.readLine());
+            hotelCost *= stayDuration;
 
-            String[] available_currencies = b.keySet().toArray(new String[0]);
+            double totalCostInUsd;//variable to store total cost in usd
+
+            totalCostInUsd = flightCost + hotelCost;// getting sum of the all costs
+
+            System.out.printf("Flight cost: USD %.2f\n", flightCost);//outputting flight cost in usd
+            System.out.printf("Hotel cost (%d days): USD %.2f\n", stayDuration, hotelCost);//outputting hotel and stay duration cost in usd
+            System.out.printf("Total: USD %.2f\n", totalCostInUsd);//outputting overall cost in usd
+
+            String[] availableCurrencies = b.keySet().toArray(new String[0]);
             System.out.print("Select your currency for final price estimation(" + String.join(", ", available_currencies) + "): ");
             String selected_currency = reader.readLine();
 
-            double final_price_local_currency = total_cost_usd * b.get(selected_currency);
+            double final_price_local_currency = totalCostInUsd * b.get(selected_currency);
 
             System.out.printf("Total in %s: %.2f\n", selected_currency, final_price_local_currency);
         } catch (IOException e) {
